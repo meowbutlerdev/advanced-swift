2d1a9107eba33fcf01928b40aeeffc08cc11be77
JISUNG PARK <meowbutlerdev@gmail.com>
2025-03-18 12:22:59 +0900

    feat: 도전 문제 1 구현
    - Base Class Car 설계
      - 브랜드, 모델, 연식: String 타입
      - 엔진: Engine 타입
    - 동작 정의
      - 운전하기: Car 주행 중... 출력
      - 기타: stop(), charge(), refuel() 등
    - Car를 상속한 ElectricCar 설계
      - 엔진: ElectricEngine 타입
    - Car를 상속한 HybridCar 설계
      - 엔진 변경: switchEngine(to:)
    - HybridCar 인스턴스 생성 후 switchEngine(to:)를 호출하여 엔진 교체 코드 작성
    - 상속으로 기능 추가 vs 프로토콜로 기능 추가의 장단점 및 차이 서술

--- AdvancedSwift/Challenge1.swift
+++ AdvancedSwift/Challenge1.swift
@@ -1,7 +1,48 @@
-//
-//  Untitled.swift
-//  AdvancedSwift
-//
-//  Created by 박지성 on 3/17/25.
-//
+protocol Engine {
+    var type: String { get }
+}
 
+struct ElectricEngine: Engine {
+    var type: String = "Electric"
+}
+
+struct HydrogenEngine: Engine {
+    var type: String = "Hydrogen"
+}
+
+
+class Car {
+    let brand: String
+    let model: String
+    let year: String
+    var engine: Engine
+
+    init(brand: String, model: String, year: String, engine: Engine) {
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.engine = engine
+    }
+
+    func drive() {
+        print("주행 중...")
+    }
+
+    func stop() {
+        print("멈춤")
+    }
+}
+
+class ElectricCar: Car {
+    init(brand: String, model: String, year: String) {
+        super.init(brand: brand, model: model, year: year, engine: ElectricEngine())
+    }
+}
+
+class HybridCar: Car {
+    func switchEngine(to engine: Engine) {
+        self.engine = engine
+    }
+}
+
+let hybridCar = HybridCar(brand: "brand", model: "model", year: "year", engine: HydrogenEngine())
--- AdvancedSwift/main.swift
+++ AdvancedSwift/main.swift
@@ -17,3 +17,27 @@ print(c(array: [1, 2, 3, 4, 5]))
 print(c(array: ["가", "나", "다", "라", "마"]))
 print(d(array: [1, 2, 3, 4, 5]))
 //print(d(array: ["가", "나", "다", "라", "마"]))
+
+// MARK: - 도전 문제 1
+print(hybridCar.engine.type)
+hybridCar.switchEngine(to: ElectricEngine())
+print(hybridCar.engine.type)
+/**
+ 상속 vs 프로토콜
+
+ 상속의 장점
+ - 코드의 재사용성이 높음
+ - 기존 기능 확장, 새로운 기능 추가 가능
+
+ 상속의 단점
+ - 다중 상속 불가능
+ - 상위클래스 수정 시 하위클래스에 영향을 미쳐 유지보수 어려워짐
+
+ 프로토콜의 장점
+ - 코드의 재사용성이 높고 유지보수가 쉬움
+ - 다중 프로토콜 채택 가능
+ - class, struct, enum 모두 사용 가능
+
+ 프로토콜의 단점
+ - 선언만 정의하므로 여러 곳에서 채택 시 동일한 메서드를 여러 곳에서 구현부를 정의해야함
+ */
